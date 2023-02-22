b3f20e2ec8eacc2070a60db3f39b4c5294b722c1
Joao Victor Negreiros <joaovictornsv@gmail.com>
2023-02-22 11:58:53 -0300

    test: use t.Run() in product test

--- product/product_test.go
+++ product/product_test.go
@@ -6,28 +6,30 @@ import (
 	. "github.com/joaovictornsv/shopping-cart-go/product"
 )
 
-func TestProductName(t *testing.T) {
-	name := "Arroz"
-	productToTest := NewProduct(name, 3.99)
+func TestProduct(t *testing.T) {
+	t.Run("GetName()", func(t *testing.T) {
+		name := "Arroz"
+		productToTest := NewProduct(name, 3.99)
 
-	productToTestName := productToTest.GetName()
-	if productToTestName != name {
-		t.Errorf("Name not match!\nExpected: \"%s\"\nReceive: \"%s\"",
-			name,
-			productToTestName,
-		)
-	}
-}
+		productToTestName := productToTest.GetName()
+		if productToTestName != name {
+			t.Errorf("Name not match!\nExpected: \"%s\"\nReceive: \"%s\"",
+				name,
+				productToTestName,
+			)
+		}
+	})
 
-func TestProductPrice(t *testing.T) {
-	price := 3.99
-	productToTest := NewProduct("Arroz", price)
+	t.Run("GetPrice()", func(t *testing.T) {
+		price := 3.99
+		productToTest := NewProduct("Arroz", price)
 
-	productToTestPrice := productToTest.GetPrice()
-	if productToTestPrice != price {
-		t.Errorf("Price not match!\nExpected: \"%f\"\nReceive: \"%f\"",
-			price,
-			productToTestPrice,
-		)
-	}
+		productToTestPrice := productToTest.GetPrice()
+		if productToTestPrice != price {
+			t.Errorf("Price not match!\nExpected: \"%f\"\nReceive: \"%f\"",
+				price,
+				productToTestPrice,
+			)
+		}
+	})
 }
